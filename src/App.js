26c5c35451aea842f51323034257da449a15d2f6
Jsgithubchannel <jsschannel99@gmail.com>
2024-05-27 09:51:40 +0900

    main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="hero-section">
+          <h1>Welcome</h1>
+          <p>Page under maintenance</p>
+        </div>
       </header>
+      <footer className="footer">
+        <p>&copy; 2024 X-Cube. All rights reserved.</p>
+      </footer>
     </div>
   );
 }
